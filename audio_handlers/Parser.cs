81905795edb6ce64bdf0edfd0f1ac435f7e7af95
zogich <tckachiksis@gmail.com>
2024-04-30 12:07:08 +1000

    Вынес классы для работы со звуком в отдельную папку

---
+++ audio_handlers/Parser.cs
@@ -0,0 +1,15 @@
+using System.Numerics;
+
+namespace test_app;
+
+class Parser{
+    List<string> list_of_commands;
+
+    public Parser(List<string> commands){
+        list_of_commands = commands;
+    }
+
+    public Vector<string> parseStringToCommands(string inpurt_string){
+        return new Vector<string>(inpurt_string.Split());
+    }
+}
