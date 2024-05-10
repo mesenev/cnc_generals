0c50a1fab3a4ab70f74be70f64ab53e61f531f82
zogich <tckachiksis@gmail.com>
2024-05-10 17:24:10 +1000

    Добавил базовый клиент для Yandex Stt

--- audio/ApiClient/Program.cs
+++ audio/ApiClient/Program.cs
@@ -1,3 +1,133 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using ApiClient;
+using Grpc.Core;
+using Grpc.Net.Client;
+using Speechkit.Stt.V3;
+using NAudio.Wave;
+
+RawAudio raw_audio = new RawAudio();
+raw_audio.AudioEncoding = RawAudio.Types.AudioEncoding.Linear16Pcm;
+raw_audio.SampleRateHertz = 44100;
+raw_audio.AudioChannelCount = 2;
+
+
+// ContainerAudio containerAudio = new ContainerAudio();
+// containerAudio.ContainerAudioType = ContainerAudio.Types.ContainerAudioType.Unspecified;
+
+AudioFormatOptions audio_format_options = new AudioFormatOptions();
+audio_format_options.RawAudio = raw_audio;
+
+TextNormalizationOptions text_normalization = new TextNormalizationOptions();
+text_normalization.TextNormalization = TextNormalizationOptions.Types.TextNormalization.Enabled;
+text_normalization.ProfanityFilter = true;
+text_normalization.LiteratureText = false;
+
+LanguageRestrictionOptions language_restriction = new LanguageRestrictionOptions();
+language_restriction.RestrictionType = LanguageRestrictionOptions.Types.LanguageRestrictionType.Whitelist;
+language_restriction.LanguageCode.Add("ru-RU");
+
+RecognitionModelOptions recognition_model = new RecognitionModelOptions();
+recognition_model.AudioFormat = audio_format_options;
+recognition_model.TextNormalization = text_normalization;
+recognition_model.LanguageRestriction = language_restriction;
+recognition_model.AudioProcessingType = RecognitionModelOptions.Types.AudioProcessingType.RealTime;
+
+StreamingOptions streaming_options =new StreamingOptions();
+streaming_options.RecognitionModel = recognition_model;
+
+StreamingRequest streamingRequestWithOptions = new StreamingRequest();
+streamingRequestWithOptions.SessionOptions = streaming_options;
+
+
+SslCredentials creds = new SslCredentials();
+
+GrpcChannelOptions channelOptions = new GrpcChannelOptions();
+channelOptions.Credentials = creds;
+
+
+GrpcChannel channel = GrpcChannel.ForAddress("https://stt.api.cloud.yandex.net:443", channelOptions);
+Recognizer.RecognizerClient recognizerClient = new Recognizer.RecognizerClient(channel);
+
+
+Metadata metadata = new Metadata();
+metadata.Add("authorization", "Bearer t1.9euelZqdjJvKmJPHkJyUkouVlZCVy-3rnpWamM2dy5eVz8aMx8-NncqKzZ3l8_c4Kg9O-e8sVVBx_t3z93hYDE757yxVUHH-zef1656Vmo6UmZbMzZGLz4vHyJPHkMia7_zF656Vmo6UmZbMzZGLz4vHyJPHkMia.JNpEl4CO48yuUR_cDVb0bIZ8hYwU_hJZxhHrkka8oV26p5sE9ZCDvy_QEH7UYj8sEsoj4SbycVIDR7o6fICXBw");
+metadata.Add("x-folder-id",  "b1gv6ij7c6frub5g0dk2");
+
+CallOptions callOptions = new CallOptions(metadata);
+
+var call = recognizerClient.RecognizeStreaming(metadata, deadline: DateTime.UtcNow.AddMinutes(5));
+call.RequestStream.WriteAsync(streamingRequestWithOptions).Wait();
+
+// await foreach (var response in call.ResponseStream.ReadAllAsync())
+// {
+//     Console.WriteLine($"s2t chunk of {response.CalculateSize()} bytes recieved ");
+//     // foreach (var chunk_rec in response)
+//     // {
+//     //     ChunkRecievedEventArgs evt = new ChunkRecievedEventArgs(chunk);
+//     //     ChunkRecived?.Invoke(null, evt);
+//     // }   
+//     
+// }
+
+WaveFormat format = new WaveFormat(44100, 2);
+WaveInEvent waveIn = new WaveInEvent();
+waveIn.WaveFormat = format;
+
+YandexSttClient apiClient = new YandexSttClient("t1.9euelZrGx8zKjZvMjpiTzpCJmZPGku3rnpWamM2dy5eVz8aMx8-NncqKzZ3l8_dYGwlO-e83PmIo_N3z9xhKBk757zc-Yij8zef1656VmpCOmZmSlcmPmo6UisqTlJvN7_zF656VmpCOmZmSlcmPmo6UisqTlJvN.G9sLwyzIcuUkOjwn5D2lzQlxBCQkaHVnH39aGfNTroKfTtfveLlnsGIRYvStg-P_AFtSHJt9F69zUrfghMKtAw", new Uri("https://stt.api.cloud.yandex.net:443"), "b1gv6ij7c6frub5g0dk2");
+apiClient.startStreamToApi();
+
+waveIn.DataAvailable += (s, a) =>
+{
+    // StreamingRequest rR = new StreamingRequest();
+    // AudioChunk audioChunk = new AudioChunk();
+    // audioChunk.Data = Google.Protobuf.ByteString.CopyFrom(a.Buffer);
+    // rR.Chunk = audioChunk;
+    // call.RequestStream.WriteAsync(rR).Wait();
+    // Console.WriteLine(a.Buffer.Length);
+    
+    apiClient.writeDataToOpenStreamCall(a.Buffer);
+    
+};
+
+
+waveIn.StartRecording();
+
+Thread.Sleep(10000);
+
+waveIn.StopRecording();
+
+apiClient.readAllDataFromResponseStream();
+apiClient.disposeAll();
+// using (FileStream fs = File.Open("test_file_ogg.opus", FileMode.Open))
+// {
+//     using (BufferedStream bs = new BufferedStream(fs, 32 * 1024))
+//     {
+//         int byteRead;
+//         byte[] buffer = new byte[32 * 1024];
+//
+//         while ((byteRead = bs.Read(buffer, 0, 32 * 1024)) > 0)
+//         {
+//             StreamingRequest rR = new StreamingRequest();
+//             AudioChunk audioChunk = new AudioChunk();
+//             audioChunk.Data = Google.Protobuf.ByteString.CopyFrom(buffer);
+//             rR.Chunk = audioChunk;
+//             call.RequestStream.WriteAsync(rR).Wait();
+//             Console.WriteLine(byteRead);
+//         }
+//     }
+// }
+// await foreach (var response in call.ResponseStream.ReadAllAsync())
+// {
+//     //Console.WriteLine($"RESPONSE Uuid : {response.SessionUuid}");
+//     if (response.Partial != null)
+//     {
+//         Console.WriteLine($"RESPONSE PARTIAL : {response.Partial}");
+//     }
+//     if (response.Final != null)
+//     {
+//         Console.WriteLine($"RESPONSE FINAL : {response.Final}");
+//     }
+//     //Console.WriteLine($"RESPONSE FINAL : {response.Final}");
+//     //Console.WriteLine($"RESPONSE audio cursor : {response.AudioCursors}");
+// }
---
+++ audio/ApiClient/YandexSttClient.cs
@@ -0,0 +1,100 @@
+﻿using System.Text.Json;
+using NAudio.Wave;
+
+namespace ApiClient;
+
+using Grpc.Core;
+using Grpc.Net.Client;
+using Speechkit.Stt.V3;
+
+public class YandexSttClient
+{
+    private string iamToken;
+    private Uri apiUri;
+    private string folderId;
+    private Recognizer.RecognizerClient recognizerClient;
+    private AsyncDuplexStreamingCall<StreamingRequest, StreamingResponse> call;
+
+    public YandexSttClient(string iam_token, Uri api_uri, string folder_id)
+    {
+        iamToken = iam_token;
+        apiUri = api_uri;
+        folderId = folder_id;
+    }
+
+    protected Metadata makeMetadata()
+    {
+        Metadata metadata = new Metadata();
+        metadata.Add("authorization", $"Bearer {iamToken}");
+        metadata.Add("x-folder-id",  folderId);
+        return metadata;
+    }
+
+    protected StreamingRequest makeRequestWithOptions()
+    {
+        RawAudio raw_audio = new RawAudio();
+        raw_audio.AudioEncoding = RawAudio.Types.AudioEncoding.Linear16Pcm;
+        raw_audio.SampleRateHertz = 44100;
+        raw_audio.AudioChannelCount = 2;
+        
+        AudioFormatOptions audio_format_options = new AudioFormatOptions();
+        audio_format_options.RawAudio = raw_audio;
+        
+        RecognitionModelOptions recognition_model = new RecognitionModelOptions();
+        recognition_model.AudioFormat = audio_format_options;
+        recognition_model.AudioProcessingType = RecognitionModelOptions.Types.AudioProcessingType.RealTime;
+        StreamingOptions streaming_options =new StreamingOptions();
+        streaming_options.RecognitionModel = recognition_model;
+        
+        StreamingRequest streamingRequestWithOptions = new StreamingRequest();
+        streamingRequestWithOptions.SessionOptions = streaming_options;
+
+        return streamingRequestWithOptions;
+    }
+
+    public void startStreamToApi()
+    {
+        SslCredentials creds = new SslCredentials();
+
+        GrpcChannelOptions channelOptions = new GrpcChannelOptions();
+        channelOptions.Credentials = creds;
+
+        GrpcChannel channel = GrpcChannel.ForAddress(apiUri, channelOptions);
+        recognizerClient = new Recognizer.RecognizerClient(channel);
+        call = recognizerClient.RecognizeStreaming(makeMetadata(), deadline: DateTime.UtcNow.AddMinutes(5));
+        call.RequestStream.WriteAsync(makeRequestWithOptions()).Wait();
+    }
+
+    public void writeDataToOpenStreamCall(byte[] bytes)
+    {
+        Console.WriteLine("Method called");
+        StreamingRequest rR = new StreamingRequest();
+        AudioChunk audioChunk = new AudioChunk();
+        audioChunk.Data = Google.Protobuf.ByteString.CopyFrom(bytes);
+        rR.Chunk = audioChunk;
+        call.RequestStream.WriteAsync(rR).Wait();
+        Console.WriteLine($"{bytes.Length} байт было отправлено в Yandex Speech Kit Recognition");
+    }
+
+    async public void readAllDataFromResponseStream()
+    {
+        
+        await foreach (var response in call.ResponseStream.ReadAllAsync())
+        {
+            //Console.WriteLine($"RESPONSE Uuid : {response.SessionUuid}");
+            if (response.Partial != null)
+            {
+                Console.WriteLine($"RESPONSE PARTIAL : {response.Partial}");
+            }
+            if (response.Final != null)
+            {
+                Console.WriteLine($"RESPONSE FINAL : {response.Final}");
+            }
+        }
+    }
+
+    public void disposeAll()
+    {
+        call.Dispose();
+    }
+}
