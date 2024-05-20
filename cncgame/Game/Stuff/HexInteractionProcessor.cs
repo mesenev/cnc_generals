5de132b38be57a70c625333d4cca6d5a9ddb4e26
Yasuhiro-gh <Pavlich_eng00@mail.ru>
2024-05-20 19:55:30 +1000

    add simple hex interaction

--- cncgame/Game/Stuff/HexInteractionProcessor.cs
+++ cncgame/Game/Stuff/HexInteractionProcessor.cs
@@ -15,11 +15,22 @@ namespace Game
 
 		public void Update(float delta, Game game)
 		{
-			// if (_cell.image) {
-			// 	_cell.image.Color = Color4.White;
-			// } else {
-			// 	_cell.image.Color = Color4.Gray;
-			// }
+			var canvasInput = game.Canvas.Input;
+			if (IsMouseOver(canvasInput.MousePosition)) {
+				_cell.image.Color = Color4.White;
+				if (canvasInput.IsKeyPressed(Key.Mouse0)) {
+					_cell.image.Color = Color4.Blue;
+				}
+			} else {
+				_cell.image.Color = Color4.Gray;
+			}
+		}
+
+		private bool IsMouseOver(Vector2 mousePos)
+		{
+			var sideSize = 20 * float.Abs(3) / 2;
+			var pixelPos = _cell.PixelPosition;
+			return Vector2.Distance(pixelPos, mousePos) < sideSize;
 		}
 	}
 }
