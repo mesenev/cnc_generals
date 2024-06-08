04bd0508101364bf28503dbf915f129b51e9ffe2
mesenev <mesenev_pr@dvfu.ru>
2024-06-08 16:25:11 +1000

    commented out camera log output

--- Game/Stuff/MoveCameraProcessor.cs
+++ Game/Stuff/MoveCameraProcessor.cs
@@ -1,47 +1,52 @@
-﻿using System;
-using Game.Widgets;
+﻿using Game.Widgets;
 using Lime;
-namespace Game.Stuff
-{
-	public class MoveCameraProcessor: IProcessor
-	{
-		private Camera2D camera;
-		public MoveCameraProcessor(Camera2D camera)
-		{
-			this.camera = camera;
-		}
-		public void Update(float delta, Game game)
-		{
-			Console.WriteLine($"CameraPosition : {camera.Position}  HexLayerSize : {CanvasManager.Instance.GetCanvas(Layers.HexMap).Size}");
-			if (Window.Current.Input.IsKeyPressed(Key.Down)) {
-				if (camera.X < 64*50 - camera.Parent.AsWidget.Size.X / 2) {
-					camera.Y += 1;
-				}
-			}
 
-			if (Window.Current.Input.IsKeyPressed(Key.Up)) {
-				if (camera.Y > camera.Parent.AsWidget.Height / 2) {
-					camera.Y -= 1;
-				}
-			}
-			if (Window.Current.Input.IsKeyPressed(Key.Left)) {
-				if (camera.X > camera.Parent.AsWidget.Width / 2 ){
-					camera.X -= 1;
-				}
-			}
-			if (Window.Current.Input.IsKeyPressed(Key.Right)) {
-				if (camera.X < 64*50) {
-					camera.X += 1;
-				}
-			}
+namespace Game.Stuff;
 
-			// if (isUpPressed) {
-			// 	camera.Y -= 1;
-			// }
-			//
-			// if (isDownPressed) {
-			// 	camera.Y += 1;
-			// }
-		}
-	}
+public class MoveCameraProcessor : IProcessor {
+    private Camera2D camera;
+
+    public MoveCameraProcessor(Camera2D camera) {
+        this.camera = camera;
+    }
+
+    public void Update(float delta, Game game) {
+        // Logger.Instance.Debug(
+        // $"CameraPosition: {camera.Position} "
+        // + $" HexLayerSize:"
+        // + $" {CanvasManager.Instance.GetCanvas(Layers.HexMap).Size}"
+        // );
+
+        if (Window.Current.Input.IsKeyPressed(Key.Down)) {
+            if (camera.X < 64 * 50 - camera.Parent.AsWidget.Size.X / 2) {
+                camera.Y += 1;
+            }
+        }
+
+        if (Window.Current.Input.IsKeyPressed(Key.Up)) {
+            if (camera.Y > camera.Parent.AsWidget.Height / 2) {
+                camera.Y -= 1;
+            }
+        }
+
+        if (Window.Current.Input.IsKeyPressed(Key.Left)) {
+            if (camera.X > camera.Parent.AsWidget.Width / 2) {
+                camera.X -= 1;
+            }
+        }
+
+        if (Window.Current.Input.IsKeyPressed(Key.Right)) {
+            if (camera.X < 64 * 50) {
+                camera.X += 1;
+            }
+        }
+
+        // if (isUpPressed) {
+        // 	camera.Y -= 1;
+        // }
+        //
+        // if (isDownPressed) {
+        // 	camera.Y += 1;
+        // }
+    }
 }
