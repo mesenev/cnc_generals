5952ac93901933c3de0fb8d10831b635edc29c35
zogich <tckachiksis@gmail.com>
2024-06-08 15:33:54 +1000

    Перенес проект CommandRecognitionModule в master

---
+++ CommandRecognitionModule/ApiClient/YandexSttClient.cs
@@ -0,0 +1,204 @@
+﻿using Google.Protobuf;
+
+namespace CommandRecognitionModule.ApiClient;
+
+using Grpc.Core;
+using Grpc.Net.Client;
+using Speechkit.Stt.V3;
+
+public class YandexSttClient {
+    private string iamToken;
+    private Uri apiUri;
+    private string folderId;
+    private Recognizer.RecognizerClient recognizerClient;
+    private AsyncDuplexStreamingCall<StreamingRequest, StreamingResponse> call;
+
+    public YandexSttClient(string iam_token, Uri api_uri, string folder_id) {
+        iamToken = iam_token;
+        apiUri = api_uri;
+        folderId = folder_id;
+    }
+
+    private Metadata makeMetadata() {
+        Metadata metadata = new Metadata();
+        metadata.Add("authorization", $"Bearer {iamToken}");
+        metadata.Add("x-folder-id", folderId);
+        return metadata;
+    }
+
+
+    //DefaultEouClassifier max_pause_between_words_hint_ms
+    private StreamingRequest makeRequestWithOptions() {
+        RawAudio raw_audio = new RawAudio();
+        raw_audio.AudioEncoding = RawAudio.Types.AudioEncoding.Linear16Pcm;
+        raw_audio.SampleRateHertz = 44100;
+        raw_audio.AudioChannelCount = 2;
+
+        AudioFormatOptions audio_format_options = new AudioFormatOptions();
+        audio_format_options.RawAudio = raw_audio;
+
+        RecognitionModelOptions recognition_model = new RecognitionModelOptions();
+        recognition_model.AudioFormat = audio_format_options;
+        recognition_model.AudioProcessingType = RecognitionModelOptions.Types.AudioProcessingType.RealTime;
+        StreamingOptions streaming_options = new StreamingOptions();
+
+        streaming_options.RecognitionModel = recognition_model;
+
+        StreamingRequest streamingRequestWithOptions = new StreamingRequest();
+        streamingRequestWithOptions.SessionOptions = streaming_options;
+
+        return streamingRequestWithOptions;
+    }
+
+    public void startStreamToApi() {
+        SslCredentials creds = new SslCredentials();
+
+        GrpcChannelOptions channelOptions = new GrpcChannelOptions();
+        channelOptions.Credentials = creds;
+
+        GrpcChannel channel = GrpcChannel.ForAddress(apiUri, channelOptions);
+        recognizerClient = new Recognizer.RecognizerClient(channel);
+        call = recognizerClient.RecognizeStreaming(makeMetadata(), deadline: DateTime.UtcNow.AddMinutes(5));
+        call.RequestStream.WriteAsync(makeRequestWithOptions()).Wait();
+    }
+
+
+    public async Task writeDataToOpenStreamCall(ByteString bytes) {
+        StreamingRequest rR = new StreamingRequest();
+        AudioChunk audioChunk = new AudioChunk();
+        audioChunk.Data = bytes;
+        rR.Chunk = audioChunk;
+        call.RequestStream.WriteAsync(rR).Wait();
+        Console.WriteLine($"{bytes.Length} байт было отправлено в Yandex Speech Kit Recognition");
+    }
+
+    // ToDo альтернатив может быть больше чем 1, нужно это учесть в этом методе
+    async public Task<string> readOneStreamingResponseFromActiveCall() {
+        string text = "";
+        while (true) {
+            if (call.ResponseStream.MoveNext().Result) {
+                Console.WriteLine(call.ResponseStream.Current.AudioCursors);
+                if (call.ResponseStream.Current.Partial != null) {
+                    if (call.ResponseStream.Current.Partial.Alternatives.Count != 0) {
+                        text = call.ResponseStream.Current.Partial.Alternatives[0].Text;
+                    }
+                }
+
+                if (call.ResponseStream.Current.Final != null) {
+                    if (call.ResponseStream.Current.Final.Alternatives.Count != 0) {
+                        text = call.ResponseStream.Current.Final.Alternatives[0].Text;
+                    }
+                }
+            }
+
+            if (text != "") {
+                break;
+            }
+        }
+
+
+        return text;
+    }
+
+    async public Task<string> ReadOneResultFromOpenStream() {
+        if (call.ResponseStream.MoveNext().Result) {
+            if (call.ResponseStream.Current.EventCase == StreamingResponse.EventOneofCase.Final) {
+                if (call.ResponseStream.Current.Final.Alternatives.Count != 0) {
+                    return call.ResponseStream.Current.Final.Alternatives[0].Text;
+                }
+            }
+
+            if (call.ResponseStream.Current.EventCase == StreamingResponse.EventOneofCase.Partial) {
+                if (call.ResponseStream.Current.Partial.Alternatives.Count != 0) {
+                    return call.ResponseStream.Current.Partial.Alternatives[0].Text;
+                }
+            }
+
+            if (call.ResponseStream.Current.EventCase == StreamingResponse.EventOneofCase.FinalRefinement) {
+                if (call.ResponseStream.Current.FinalRefinement.NormalizedText.Alternatives.Count != 0) {
+                    return call.ResponseStream.Current.FinalRefinement.NormalizedText.Alternatives[0].Text;
+                }
+            }
+        }
+
+        return "";
+    }
+
+    async public Task<string> ReadFinalRefirementResponseFromOpenStream() {
+        if (call.ResponseStream.MoveNext().Result) {
+            if (call.ResponseStream.Current.EventCase == StreamingResponse.EventOneofCase.FinalRefinement) {
+                return call.ResponseStream.Current.FinalRefinement.NormalizedText.Alternatives[0].Text;
+            }
+        }
+
+        return "";
+    }
+
+    async public Task<string> readFirstResponseWithContentInStream() {
+        await foreach (var response in call.ResponseStream.ReadAllAsync()) {
+            if (response.EventCase == StreamingResponse.EventOneofCase.Final) {
+                if (response.Final.Alternatives.Count != 0) {
+                    return response.Final.Alternatives[0].Text;
+                }
+            }
+
+            if (response.EventCase == StreamingResponse.EventOneofCase.Partial) {
+                if (response.Partial.Alternatives.Count != 0) {
+                    return response.Partial.Alternatives[0].Text;
+                }
+            }
+        }
+
+        throw new Exception("No final recognize result has been return");
+    }
+
+    async public Task readAllDataFromResponseStream() {
+        Console.WriteLine("START READ ALL DATA");
+        await foreach (var response in call.ResponseStream.ReadAllAsync()) {
+            //Console.WriteLine($"RESPONSE Uuid : {response.SessionUuid}");
+            //Console.WriteLine($"CURSOR : {response.AudioCursors}");
+            if (response.Partial != null) {
+                foreach (var alternative in response.Partial.Alternatives) {
+                    Console.WriteLine($"RESPONSE PARTIAL : {alternative.Text}");
+                }
+            }
+
+            if (response.Final != null) {
+                foreach (var alternative in response.Final.Alternatives) {
+                    Console.WriteLine($"RESPONSE FINAL : {alternative.Text}");
+                }
+            }
+
+            if (response.FinalRefinement != null) {
+                foreach (var alternative in response.FinalRefinement.NormalizedText.Alternatives) {
+                    Console.WriteLine($"FINAL REFIREMENT : {alternative.Text}");
+                }
+            }
+        }
+    }
+
+    async public Task<string> ReadLastResultInStream() {
+        string last_result_in_stream = "";
+        await foreach (var response in call.ResponseStream.ReadAllAsync()) {
+            if (response.EventCase == StreamingResponse.EventOneofCase.Final) {
+                if (response.Final.Alternatives.Count != 0 && response.Final.Alternatives[0].Text != "") {
+                    last_result_in_stream = response.Final.Alternatives[0].Text;
+                }
+            }
+
+            if (response.EventCase == StreamingResponse.EventOneofCase.Partial) {
+                if (response.Partial.Alternatives.Count != 0 && response.Partial.Alternatives[0].Text != "") {
+                    last_result_in_stream = response.Partial.Alternatives[0].Text;
+                }
+            } else if (last_result_in_stream != "") {
+                return last_result_in_stream;
+            }
+        }
+
+        return last_result_in_stream;
+    }
+
+    public void disposeAll() {
+        call.Dispose();
+    }
+}
---
+++ CommandRecognitionModule/Services/SpeechToCommandService.cs
@@ -0,0 +1,77 @@
+﻿using System.Text;
+using CommandRecognitionModule.ApiClient;
+using Grpc.Core;
+
+namespace CommandRecognitionModule.Services;
+
+public class SpeechToCommandService : SpeechToCommand.SpeechToCommandBase {
+    private readonly ILogger<SpeechToCommandService> _logger;
+    private readonly YandexSttClient _yandexSttClient;
+    private readonly string iamToken;
+
+    public SpeechToCommandService(ILogger<SpeechToCommandService> logger) {
+        _logger = logger;
+        iamToken =
+            "t1.9euelZrIyYqdkpXKm8bOj5OblpqXj-3rnpWamM2dy5eVz8aMx8-NncqKzZ3l8_ceVTVN-e9Xekpn_d3z914DM03571d6Smf9zef1656VmpeVl8aTlYyPjMiZzY2YzIuV7_zF656VmpeVl8aTlYyPjMiZzY2YzIuV.iZag2k_Y9-G9X4ub9-ujIQ23Ftn_EFvYHEw6x5eQRtZ7nYktSJQGqeqr4lSuEhROoVRm0zBMDMElbgCAYV5oBA";
+        _yandexSttClient = new YandexSttClient(iamToken, new Uri("https://stt.api.cloud.yandex.net:443"),
+            "b1gv6ij7c6frub5g0dk2");
+        _yandexSttClient.startStreamToApi();
+    }
+
+    public override async Task<Response> AudioToText(IAsyncStreamReader<VoiceAudio> requestStream,
+        ServerCallContext context) {
+        await foreach (VoiceAudio audio_voice in requestStream.ReadAllAsync()) {
+            try {
+                _yandexSttClient.writeDataToOpenStreamCall(audio_voice.RecordVoice).Wait();
+            }
+            //Если ytt deadline истек, пересоздает поток к ytt, и записывает в него последнее полученное сообщение
+            catch (Exception error) {
+                Console.WriteLine("Exception during write to yandex stt. Trying reconnect");
+                _yandexSttClient.disposeAll();
+                _yandexSttClient.startStreamToApi();
+                _yandexSttClient.writeDataToOpenStreamCall(audio_voice.RecordVoice).Wait();
+            }
+        }
+
+        return new Response { Content = "Working" };
+    }
+
+    public override async Task TextToCommand(
+        InputText request,
+        IServerStreamWriter<DummyCommand> responseStream,
+        ServerCallContext context) {
+        List<string> known_commands = new List<string>(["вверх", "вниз", "влево", "вправо"]);
+        CommandParser parser = new CommandParser(known_commands);
+        Console.WriteLine("START TEXT TO COMMAND SERVICE");
+        while (true) {
+            Console.WriteLine("ITERATION");
+            string recString = "";
+            try {
+                recString = _yandexSttClient.ReadFinalRefirementResponseFromOpenStream().Result;
+            } catch (Exception error) {
+                _yandexSttClient.disposeAll();
+                _yandexSttClient.startStreamToApi();
+                Console.WriteLine(error);
+                Console.WriteLine(
+                    "Exception during read final refirement from yandex stt. You should write something to request stream of CRM to recreate stream to YSTT");
+            }
+
+            //recString = Encoding.Default.GetString(Encoding.Default.GetBytes(recString));
+            Console.WriteLine($"REC STRING: {recString}");
+            if (recString.Split().Length >= 2) {
+                try {
+                    int offset = Int32.Parse(recString.Split()[1]);
+                    await responseStream.WriteAsync(new DummyCommand {
+                        Direction = recString.Split()[0], Offset = offset
+                    });
+                } catch (FormatException e) {
+                    Console.WriteLine("");
+                }
+            }
+            //else
+            // {
+            //     await responseStream.WriteAsync(new DummyCommand{ Direction = "EMPTY ANSWER", Offset = 0 });
+            // }
+        }
+    }
+}
